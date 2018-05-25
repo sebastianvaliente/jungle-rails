b2f760714bc179bfa06c03b17b9b81b27dafe326
Sebastian V <svnineseven@gmail.com>
2018-05-25 19:33:37 +0000

    added admin security so that credentials are needed to access admin controls

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,7 @@
 class Admin::CategoriesController < ApplicationController
 
+  http_basic_authenticate_with name: ENV["ADMIN_NAME"], password: ENV["ADMIN_PASS"], except: :index
+
   def index
   end
 
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with name: ENV["ADMIN_NAME"], password: ENV["ADMIN_PASS"], except: :index
+
   def index
     @products = Product.order(id: :desc).all
   end
